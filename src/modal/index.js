8332493f480a434398a953a8447cffcbad20f399
Ju99ernaut <brendonzee@gmail.com>
2021-03-18 18:43:47 +0200

    Fix update

--- src/modal/index.js
+++ src/modal/index.js
@@ -1,28 +1,29 @@
-import TemplateManager from './components';
+import TemplateManager from './manager';
 
 export default (editor, opts = {}) => {
     const { $ } = editor;
     const mdl = editor.Modal;
     const sm = editor.StorageManager;
+    const pfx = editor.getConfig('stylePrefix');
     const mdlClass = `${pfx}mdl-dialog-tml`;
     let templateManager = null;
 
-    cm.add('open-pages', {
+    editor.Commands.add('open-templates', {
         run(editor, sender) {
             const mdlDialog = document.querySelector(`.${pfx}mdl-dialog`);
             mdlDialog.classList.add(mdlClass);
             sender && sender.set && sender && sender.set('active');
-            !templateManager && (templateManager = new TemplateManager)
+            !templateManager && (templateManager = new TemplateManager(editor, opts))
             mdl.setTitle(opts.mdlTitle);
             mdl.setContent($('.lds-ellipsis').length ? $('.lds-ellipsis') : templateManager.loader());
-            sm.get(sm.getCurrent()).loadAll(res => {
+            sm.getCurrentStorage().loadAll(res => {
                     let content = $('#pages');
                     content = content.length ? content : $(templateManager.render());
                     mdl.setContent(content);
                     const pages = content.find('#pages-container');
                     const templates = content.find('#templates-container');
-                    pages.remove(`${this.pfx}templates-card`);
-                    templates.remove(`${this.pfx}templates-card`);
+                    pages.find(`.${this.pfx}templates-card`).remove();
+                    templates.find(`.${this.pfx}templates-card`).remove();
                     pages.append(templateManager.update(res.filter(r => !r.template)));
                     templates.append(templateManager.update(res.filter(r => r.template)), false);
                 },
