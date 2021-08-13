e5b057b51b966ca6b30a551bbcb513ec4af874d9
Ju99ernaut <brendonzee@gmail.com>
2021-08-13 13:25:24 +0200

    Add templates

--- src/manager.js
+++ src/manager.js
@@ -38,9 +38,16 @@ export default class TemplateManager {
         this.state = { ...this.state, ...state };
     }
 
+    sm() {
+        return this.editor.Storage;
+    }
+
+    cs() {
+        return this.editor.Storage.getCurrentStorage();
+    }
+
     onRender() {
-        const { editor, setState } = this;
-        const sm = editor.StorageManager;
+        const { setState, sm } = this;
 
         /* Set request loading state */
         setState({
@@ -48,7 +55,7 @@ export default class TemplateManager {
         });
 
         /* Fetch sites from storage API */
-        sm.getCurrentStorage().loadAll(sites => {
+        sm().getCurrentStorage().loadAll(sites => {
             /* Set sites and turn off loading state */
             setState({
                 sites,
@@ -94,23 +101,21 @@ export default class TemplateManager {
     }
 
     handleOpen(e) {
-        const { editor } = this;
+        const { editor, cs } = this;
         const { projectId } = this.state;
-        const cs = editor.Storage.getCurrentStorage();
-        cs.setIdx(projectId);
+        cs().setIdx(projectId);
         editor.load(res => {
             editor.setComponents(res.components ? JSON.parse(res.components) : res.html);
             editor.setStyle(res.styles ? JSON.parse(res.styles) : res.css);
-            cs.setThumbnail(res.thumbnail || '');
-            cs.setIsTemplate(res.template);
+            cs().setThumbnail(res.thumbnail || '');
+            cs().setIsTemplate(res.template);
             editor.Modal.close();
         });
     }
 
     handleCreate(e) {
-        const { editor } = this;
+        const { editor, cs } = this;
         const { projectId, nameText } = this.state;
-        const cs = editor.Storage.getCurrentStorage();
         // TODO if no projectId generate blank project
         // TODO if no nameText use uuid
         //const data = {};
@@ -123,14 +128,14 @@ export default class TemplateManager {
         //    thumbnail: '',
         //    ...data
         //});
-        cs.setIdx(projectId);
-        cs.setIsTemplate(false);
+        cs().setIdx(projectId);
+        cs().setIsTemplate(false);
         editor.load(res => {
             editor.setComponents(res.components ? JSON.parse(res.components) : res.html);
             editor.setStyle(res.styles ? JSON.parse(res.styles) : res.css);
-            cs.setId(nameText);
-            cs.setIdx(editor.runCommand('get-uuidv4'));
-            cs.setThumbnail(res.thumbnail || '');
+            cs().setId(nameText);
+            cs().setIdx(editor.runCommand('get-uuidv4'));
+            cs().setThumbnail(res.thumbnail || '');
             editor.Modal.close();
         });
         // TODO reset sites
@@ -138,7 +143,7 @@ export default class TemplateManager {
 
     handleEdit(e) {
         const input = this.$el?.find('input.edit');
-        this.editor.Storage.getCurrentStorage().update({
+        this.cs().update({
             idx: e.currentTarget.dataset.idx,
             id: input.val().trim(),
             description: '',
@@ -150,9 +155,8 @@ export default class TemplateManager {
     }
 
     handleDelete(e) {
-        const { editor, opts } = this;
-        editor.Storage.getCurrentStorage()
-            .delete(opts.onDelete, opts.onDeleteError, e.currentTarget.dataset.idx);
+        const { cs, opts } = this;
+        cs().delete(opts.onDelete, opts.onDeleteError, e.currentTarget.dataset.idx);
         // TODO reset sites
     }
 
@@ -353,6 +357,239 @@ export default class TemplateManager {
             </div>`);
         cont.find('.header').on('click', this.handleSort);
         cont.find('#pages, #templates').on('click', this.handleTabs);
+
+        this.$el = cont;
+        return cont;
+    }
+}
+
+export class PagesApp {
+    constructor(editor, opts = {}) {
+        this.editor = editor;
+        this.$ = editor.$;
+        this.pfx = editor.getConfig('stylePrefix');
+        this.id = editor.Storage.getConfig().id || 'gjs-';
+        this.opts = opts;
+        this.onRender = this.onRender.bind(this);
+        this.addPage = this.addPage.bind(this);
+        this.selectPage = this.selectPage.bind(this);
+        this.removePage = this.removePage.bind(this);
+
+        /* Set initial app state */
+        this.state = {
+            pages: [],
+            loading: false
+        };
+    }
+
+    pm() {
+        return editor.Pages;
+    }
+
+    setState(state) {
+        this.state = { ...this.state, ...state };
+        this.update();
+    }
+
+    onRender() {
+        const { pm, setState, editor } = this;
+        setState({
+            loading: true
+        });
+        setState({
+            pages: [...pm().getAll()]
+        });
+        editor.on('page', () => {
+            setState({
+                pages: [...pm().getAll()]
+            })
+        });
+        setState({
+            loading: false
+        });
+    }
+
+    isSelected(page) {
+        return this.pm().getSelected().id === page.id;
+    }
+
+    selectPage(e) {
+        this.pm().select(e.currentTarget.dataset.key);
+        this.update();
+    }
+
+    removePage(e) {
+        this.pm().remove(e.currentTarget.dataset.key);
+        this.update();
+    }
+
+    addPage() {
+        const { pm } = this;
+        const len = pm().getAll().length;
+        pm().add({
+            name: `Page ${len + 1}`,
+            component: ''
+        });
+        this.update();
+    }
+
+    renderPagesList() {
+        const { pages, loading } = this.state;
+        const { opts, isSelected } = this;
+
+        if (loading) return opts.loader || '<div>Loading pages...</div>';
+
+        return pages.map((page, i) => `<div 
+                data-idx="${i}" 
+                data-key="${page.id}"  
+                class="page ${isSelected(page) ? 'selected' : ''}"
+            >
+                ${page.get('name') || page.id}
+                ${isSelected(page) ? '<span class="page-close" data-key="${page.id}">&Cross</span>' : ''}
+            </div>`);
+    }
+
+    update() {
+        this.$el?.find('.pages').html(this.renderPagesList());
+        this.$el?.find('.page').on('click', this.selectPage);
+        this.$el?.find('.page-close').on('click', this.removePage);
+    }
+
+    render() {
+        const { $ } = this;
+
+        // Do stuff on render
+        this.onRender();
+        this.$el?.remove();
+
+        const cont = $(`<div class="pages-wrp">
+                <div  class="flex-row">
+                    <input class="tm-input" type="text" placeholder="page name" />
+                </div>
+                <div class="add-page">Add New Page</div>
+                <div class="pages">
+                    ${this.renderPagesList()}
+                </div>
+            </div>`);
+        cont.find('.add-page').on('click', this.addPage);
+
+        this.$el = cont;
+        return cont;
+    }
+}
+
+export class ProjectData {
+    constructor(editor, opts = {}) {
+        this.editor = editor;
+        this.$ = editor.$;
+        this.pfx = editor.getConfig('stylePrefix');
+        this.id = editor.Storage.getConfig().id || 'gjs-';
+        this.opts = opts;
+        this.onRender = this.onRender.bind(this);
+        this.setState = this.setState.bind(this);
+        this.handleTabs = this.handleTabs.bind(this);
+        this.handleSave = this.handleSave.bind(this);
+        this.handleThumbnail = this.handleThumbnail.bind(this);
+
+        /* Set initial app state */
+        this.state = {
+            currentPageId: '',
+            tab: 'page',
+            pageData: {},
+            projectData: {},
+            loading: false
+        };
+    }
+
+    setState(state) {
+        this.state = { ...this.state, ...state };
+        this.update();
+    }
+
+    setStateSilent(state) {
+        this.state = { ...this.state, ...state };
+    }
+
+    update() {
+        this.$el?.find('#setings').html(this.renderSettings());
+        this.$el?.find('#save').on('click', this.handleSave);
+    }
+
+    onRender() {
+        const { setState } = this;
+        setState({
+            loading: true
+        });
+        // TODO Setup
+        setState({
+            loading: false
+        });
+    }
+
+    handleTabs(e) {
+        const { target } = e;
+        const { $el, pfx, $ } = this;
+        $el.find(`.${pfx}tablinks`).removeClass('active');
+        $(target).addClass('active');
+        if (target.id === 'page') {
+            this.setState({ tab: 'page' });
+        } else {
+            this.setState({ tab: 'project' });
+        }
+    }
+
+    handleSave(e) { }
+
+    handleThumbnail(e) { }
+
+    renderSettings() {
+        const { tab, loading } = this.state;
+        const { opts, pfx } = this;
+
+        if (loading) return opts.loader || '<div>Loading pages...</div>';
+
+        if (tab === 'page') {
+            return `<div class="flex-row">
+                <input class="name tm-input" placeholder="Current page name"/>
+            </div>`
+        } else {
+            return `<div class="${pfx}tip-about ${pfx}four-color">Enter url, select from asset manager or generate thumbnail.</div>
+            <div class="flex-row">
+                <input class="thumbnail tm-input" placeholder="Project thumbnail"/>
+                <button id="assets" class="primary-button">Assets</button>
+                <button id="generate" class="primary-button">Generate</button>
+            </div>
+            <div class="flex-row">
+                <input class="name tm-input" placeholder="Project name"/>
+            </div>
+            <div class="flex-row">
+                <input class="desc tm-input" placeholder="Project description"/>
+            </div>
+            <div class="flex-row">
+                <input class="template tm-input" type="checkbox"/>
+            </div>`
+        }
+    }
+
+    render() {
+        const { $, pfx } = this;
+
+        // Do stuff on render
+        this.onRender();
+        this.$el?.remove();
+
+        const cont = $(`<div class="contents">
+                <div class="${pfx}tab">
+                    <button id="page" class="${pfx}tablinks active">Page</button>
+                    <button id="proejct" class="${pfx}tablinks">Project</button>
+                </div>
+                <div id="settings">
+                    ${this.renderSettings()}
+                </div>
+                <div class="flex-row">
+                    <button id="save" class="primary-button">Save</button>
+                </div>
+            </div>`);
 
         this.$el = cont;
         return cont;
